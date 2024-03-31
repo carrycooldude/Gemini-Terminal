d817ada9a13c148f9be62b6241087ffc4686930e
Kartikey Rawat <41143496+carrycooldude@users.noreply.github.com>
2024-03-31 13:44:46 +0530

    Added

---
+++ gemini-start.js
@@ -0,0 +1,19 @@
+const dotenv = require('dotenv');
+import { GoogleGenerativeAI } from "@google/generative-ai";
+
+dotenv.config();
+
+const genAI = new GoogleGenerativeAI(process.env.API_KEY);
+
+async function run(){
+    const model = genAI.getGenerativeModel({
+        model:"gemini-pro"
+    });
+    const prompt = "Write a Rap with the pen writing of an Indian Rapper KR$NA"
+    const result = await model.generateContent(prompt);
+    const response = await result.response();
+    const text = response.text();
+    console.log(text);
+}
+
+run();
